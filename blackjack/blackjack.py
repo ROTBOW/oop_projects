64d3893eba54990aa0eb5b43dfebcf413191e0ce
Josiah_L <sjosiahleon@gmail.com>
2022-07-28 07:32:23 -0700

    started blackjack

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,6 +1,33 @@
+import random
+class Card:
 
+    def __init__(self, value, suit) -> None:
+        self.value = value
+        self.suit = suit
 
-class blackjack:
+    @classmethod
+    def create_deck(self):
+        deck = []
+        suits = {'hearts', 'spades', 'clovers', 'diamonds'}
+        for suit in suits:
+            for num in range(1, 13):
+                deck.append(
+                    Card(num, suit)
+                )
+        return deck
+
+    @classmethod
+    def shuffle_desk(self, deck):
+        return random.shuffle(deck)
+
+class Blackjack:
     
     def __init__(self) -> None:
-        pass
+        self.deck = Card.create_deck()
+        self.player_hand = []
+        self.cpu_hand = []
+
+    def __shuffle(self):
+        self.deck = Card.shuffle_desk(self.deck)
+
+    
