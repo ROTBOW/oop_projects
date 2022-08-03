d78002b421d3f62d0b516446720bb232f4426cc5
Josiah Leon <sjosiahleon@gmail.com>
2022-08-03 13:36:16 -0700

    added show hand & set up game loop.

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -10,7 +10,7 @@ class Card:
         deck = []
         suits = {'hearts', 'spades', 'clovers', 'diamonds'}
         for suit in suits:
-            for num in range(1, 13):
+            for num in range(1, 14):
                 deck.append(
                     Card(num, suit)
                 )
@@ -26,6 +26,8 @@ class Blackjack:
         self.deck = Card.create_deck()
         self.player_hand = []
         self.cpu_hand = []
+        self.turn = True
+        self.winner = ''
 
     def __shuffle(self):
         self.deck = Card.shuffle_desk(self.deck)
@@ -34,3 +36,32 @@ class Blackjack:
         player = self.player_hand if player == 'player' else self.cpu_hand
         player.append(self.deck.pop())
     
+    def show_hand(self):
+        cards = {
+            1: 'Ace',
+            11: 'Jack',
+            12: 'Queen',
+            13: 'King'
+        }
+        print(', '.join(
+            [str(cards.get(x.value, x.value)) for x in self.player_hand]
+            ))
+        
+
+    def play(self):
+        print('BlackJack Begin.')
+        self.deck = Card.create_deck()
+        self.__shuffle()
+        while self.turn:
+            self.show_hand()
+            self.action() # needs to be made
+        self.cpu_move() # needs to be made
+        self.check_winner() # needs to be made
+        print(f'Game Over.\nWinner is {self.winner}')
+
+# game = Blackjack()
+# game.draw('player')
+# game.draw('player')
+# game.draw('player')
+# game.draw('player')
+# game.show_hand()
